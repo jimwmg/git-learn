bd205942c9605ab87a5cc75548d23cf032013ca5
JiM-W <550116990@qq.com>
2019-12-09 15:52:30 +0800

    my-type: 更新readme

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,15 +1,15 @@
-module.exports = {extends: ['@commitlint/config-angular']};
+// module.exports = {extends: ['@commitlint/config-angular']};
 //也可以自己定义type
-// module.exports = {
-//   extends: ['@commitlint/config-conventional'],
-//   rules: {
-//     'type-enum': [2, 'always', [
-//       "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
-//     ]],
-//     'subject-full-stop': [0, 'never'],
-//     'subject-case': [0, 'never']
-//   }
-// };
+module.exports = {
+  // extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', ["my-type",
+      "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
+    ]],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never']
+  }
+};
 
 //[build, ci, docs, feat, fix, perf, refactor, revert, style, test]
 

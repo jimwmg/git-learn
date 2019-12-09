33bc549b6e6db5826bcec7e5c3eae2a14d4af626
JiM-W <550116990@qq.com>
2019-12-09 15:52:22 +0800

    my-type: 更新readme

--- commitlint.config.js
+++ commitlint.config.js
@@ -11,20 +11,7 @@ module.exports = {extends: ['@commitlint/config-angular']};
 //   }
 // };
 
+//[build, ci, docs, feat, fix, perf, refactor, revert, style, test]
 
 
 
-/*
-type 取值
-Must be one of the following:
-
-build: Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)
-ci: Changes to our CI configuration files and scripts (example scopes: Circle, BrowserStack, SauceLabs)
-docs: Documentation only changes
-feat: A new feature
-fix: A bug fix
-perf: A code change that improves performance
-refactor: A code change that neither fixes a bug nor adds a feature
-style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)
-test: Adding missing tests or correcting existing tests
-*/

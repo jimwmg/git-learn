bec5ec2774aa32d4c41a1dec5ab5840f8a51a916
JiM-W <550116990@qq.com>
2019-12-09 17:28:27 +0800

    feat: update versionb to 1.0

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 // module.exports = {extends: ['@commitlint/config-angular']};
 //也可以自己定义type 比如：git commit -m"my-type: 更新readme"
 module.exports = {
-  // extends: ['@commitlint/config-angular'],
+  extends: ['@commitlint/config-angular'],
   rules: {
     'type-enum': [2, 'always', ["my-type",
       "feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"
